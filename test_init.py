4f4b109ccdcd63c7dee38dafc584720a16eb8823
Malav Naik <malavnaik12@gmail.com>
2024-07-08 22:50:01 -0500

    installed and setup an example code block to check mlflow

--- test_init.py
+++ test_init.py
@@ -1,4 +1,13 @@
 import torch
+import mlflow
+
 print(torch.cuda.is_available())
 x = torch.rand(5, 3)
 print(x)
+
+mlflow.start_run()
+mlflow.log_param("my", "param")
+mlflow.log_metric("score", 100)
+run = mlflow.active_run()
+print("Active run_id: {}".format(run.info.run_id))
+mlflow.end_run()
