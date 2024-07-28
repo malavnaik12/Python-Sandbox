0d4f7be10c6121be06f4549a5316ab33a10d46ac
Malav Naik <malavnaik12@gmail.com>
2024-07-27 21:16:00 -0500

    connected a simple flask backend with vue frontend

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <UserGreeting />
+  </div>
+</template>
+
+<script>
+import UserGreeting from './components/UserGreeting.vue';
+
+export default {
+  name: 'App',
+  components: {
+    UserGreeting
+  }
+};
+</script>
